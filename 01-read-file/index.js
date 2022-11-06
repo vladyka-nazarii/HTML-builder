476777d19144ddb0cf64ae0347b1f6c6bb75bb1e
Nazarii Vladyka <vladyka.nazarii@gmail.com>
2022-11-06 19:29:31 +0200

    feat: resolve 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const path = require('path');
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+stream.on('data', data => console.log(data));
