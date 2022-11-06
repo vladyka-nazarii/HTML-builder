8eea60baaf3df402d299c4e5de768c1ef8aa1cf2
Nazarii Vladyka <vladyka.nazarii@gmail.com>
2022-11-07 01:19:37 +0200

    feat: resolve 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,19 +2,22 @@ const fs = require('fs');
 const path = require('path');
 const source = path.join(__dirname, 'styles');
 const destination = path.join(__dirname, 'project-dist', 'bundle.css');
-async function mergeCSS(from, to) {
+function mergeCSS(from, to) {
   try {
+    const writeStream = fs.createWriteStream(to, 'utf-8');
     fs.readdir(from, {withFileTypes: true}, (err, files) => {
       if (err) throw err;
-      files.forEach(async element => {
-        if (element.isFile()) {
-          await fs.promises.copyFile(path.join(from, element.name), path.join(to, element.name));
+      files.forEach(element => {
+        if (element.isFile() && path.extname(element.name) === '.css') {
+          const elementPath = path.join(from, element.name);
+          const readStream = fs.createReadStream(elementPath);
+          readStream.pipe(writeStream);
         };
       })
     });
-    console.log('The folder was copied successfully');
+    console.log('Files was merged successfully');
   } catch {
-    console.log('The folder could not be copied');
+    console.log('Files could not be merged');
   };
 };
 mergeCSS(source, destination);
